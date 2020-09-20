791b9e3e67c10971d18968c8c34ef1fd7fbf63be
deniarianto1606 <deni.arianto1606@gmail.com>
2020-09-20 21:00:33 +0700

    add function delete_first, add_first, get_in, set_in and insert_in

--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -10,6 +10,15 @@ class SinglyLinkedList():
         self.tail = tail
         self.length = length
 
+    def print(self):
+        text_to_print = ""
+        temp = self.head
+        while temp.next_node is not None:
+            text_to_print += str(temp.data) + ", "
+            temp = temp.next_node
+        text_to_print += str(temp.data)
+        print(text_to_print)
+
     # push : insert last.
     # push o(1) : we can push directly to tail,
     # push o(n) : when we didn't have a tail.
@@ -64,6 +73,48 @@ class SinglyLinkedList():
         self.length += 1
         return self.head
 
+    # o(n): we need to traverse until position of index
+    def get_in(self, index):
+        counter = 0
+        if index > self.length or index < 0:
+            return None
+        temp = self.head
+        while counter != index:
+            temp = temp.next_node
+            counter += 1
+        return temp
+
+    # o(n): we need to traverse until position of index
+    # set_in will replace data in index n
+    def set_in(self, data, index):
+        temp = self.get_in(index)
+        temp.data = data
+        return self.head
+
+    # o(n): we need to traverse until position of index
+    # set_in will replace data in index n
+    # need temp and current node to change the pointer
+    # insert_in will shift current index to become next node for new data
+    def insert_in(self, data, index):
+        if index > self.length or index < 0:
+            return None
+        self.length += 1
+        if index == 0: return self.add_first(data)
+        if self.length == index: return self.push(data)
+        counter = 0
+        temp = self.head
+        current = temp
+        while counter != index:
+            current = temp
+            temp = temp.next_node
+            counter += 1
+        new_node = Node(data)
+        current.next_node = new_node
+        new_node.next_node = temp
+        return self.head
+
+
+
 
 def test_push():
     singly = SinglyLinkedList()
@@ -101,7 +152,36 @@ def test_add_first():
     print()
 
 
+def test_get_in():
+    singly = SinglyLinkedList()
+    singly.push(9)
+    singly.push(1)
+    singly.push(3)
+    singly.get_in(-1)
+
+def test_set_in():
+    singly = SinglyLinkedList()
+    singly.push(9)
+    singly.push(1)
+    singly.push(3)
+    singly.set_in(100, 1)
+    #singly.print()
+
+
+def test_insert_in():
+    singly = SinglyLinkedList()
+    singly.push(9)
+    singly.push(1)
+    singly.push(3)
+    singly.insert_in(100, 2)
+    singly.insert_in(99, 0)
+    singly.insert_in(77, 5)
+    singly.print()
+
 test_push()
 test_pop()
 test_delete_first()
 test_add_first()
+test_get_in()
+test_set_in()
+test_insert_in()
